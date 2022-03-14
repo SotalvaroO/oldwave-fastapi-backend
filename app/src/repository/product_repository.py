2fd1ec16bf52ac368a84286e7a2ae9a74641905a
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-13 23:53:58 -0500

    Reestructuración de los paquetes
    Se movieron los paquetes a un nuevo paquete llamado source, para agregar el testing posteriormente

--- app/src/repository/product_repository.py
+++ app/src/repository/product_repository.py
@@ -1,6 +1,6 @@
 from unicodedata import name
-from app.config.db_connection import product_collection
-from app.model.product_entity import ProductEntity, ProductDetailEntity
+from app.src.config.db_connection import product_collection
+from app.src.model.product_entity import ProductEntity, ProductDetailEntity
 
 async def get_product_by_name_like(name: str):
     document = await product_collection.find({"name": '/{name}/'})
