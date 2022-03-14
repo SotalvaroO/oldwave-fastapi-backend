8f0ed9b936e975a8f60afacde266a0c6aed3c9cb
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-14 12:36:13 -0500

    Corrección de la capa repositorio

--- app/src/repository/product_repository.py
+++ app/src/repository/product_repository.py
@@ -3,8 +3,13 @@ from app.src.config.db_connection import product_collection
 from app.src.model.product_entity import ProductEntity, ProductDetailEntity
 
 async def get_product_by_name_like(name: str):
-    document = await product_collection.find({"name": '/{name}/'})
-    return document
+    products = []
+    
+    cursor = product_collection.find({"name":{"$regex": name, "$options": "i"}}) #/.*I.*/i
+    async for  document in cursor:
+        print(document)
+        products.append(ProductEntity(**document))
+    return products
 
 async def get_product_detail(code:str):
     document = await product_collection.find_one({"product_code": code})
