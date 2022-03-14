2d4c615f16340e5daf677cad5008b92edda59ce7
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-14 12:56:42 -0500

    Capa repositorio
    Agregación a la capa repositorio para buscar productos por nombre

--- app/src/repository/product_repository.py
+++ app/src/repository/product_repository.py
@@ -3,8 +3,11 @@ from app.src.config.db_connection import product_collection
 from app.src.model.product_entity import ProductEntity, ProductDetailEntity
 
 async def get_product_by_name_like(name: str):
-    document = await product_collection.find({"name": '/{name}/'})
-    return document
+    products = []
+    cursor = product_collection.find({"name":{"$regex": name, "$options": "i"}})
+    async for document in cursor:
+        products.append(ProductEntity(**document))
+    return products
 
 async def get_product_detail(code:str):
     document = await product_collection.find_one({"product_code": code})
