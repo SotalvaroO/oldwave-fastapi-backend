fab31133d31813e82367b5f8c40adccd03d172ca
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-14 15:30:48 -0500

    Agregación de status codes
    Se mejoraron las rutas

--- app/src/routes/product_routes.py
+++ app/src/routes/product_routes.py
@@ -1,18 +1,20 @@
 
-from fastapi import APIRouter, HTTPException
-from app.src.repository import product_repository
-from app.src.model.product_entity import ProductEntity, ProductDetailEntity
+from fastapi import APIRouter, HTTPException, status
+from app.src.service import product_service
+from app.src.model.product_entity import ProductDetailEntity
 
 route = APIRouter(prefix='/product', tags=['Product'])
 
 @route.get('')
 async def get_products_by_name(name: str):
-    response = await product_repository.get_product_by_name_like(name)
-    return response
+    products = await product_service.list_products_by_name_like(name)
+    if products:
+        return products
+    raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
 
 @route.get('/detail/{code}', response_model=ProductDetailEntity)
 async def get_product_detail_by_code(code : str):
-    response = await product_repository.get_product_detail(code)
+    response = await product_service.find_product_by_code(code)
     if response:
         return response
-    raise HTTPException(404, "No se encontró el producto con el código {code}")
+    raise HTTPException(status_code=404, detail =f"No existe el producto con código {code}")
