3b115136d8f9bb5c786b8350cc1afc5790dac85f
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-16 14:30:51 -0500

    Cambio en los metodos
    cambio de tipo async a sync

--- app/src/routes/product_routes.py
+++ app/src/routes/product_routes.py
@@ -6,15 +6,15 @@ from app.src.model.product_entity import ProductDetailEntity
 route = APIRouter(prefix='/product', tags=['Product'])
 
 @route.get('')
-async def get_products_by_name(name: str):
-    products = await product_service.list_products_by_name_like(name)
+def get_products_by_name(name: str):
+    products = product_service.list_products_by_name_like(name)
     if products:
         return products
     raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
 
 @route.get('/{code}/detail/', response_model=ProductDetailEntity)
-async def get_product_detail_by_code(code : str):
-    response = await product_service.find_product_by_code(code)
+def get_product_detail_by_code(code : str):
+    response = product_service.find_product_by_code(code)
     if response:
         return response
     raise HTTPException(status_code=404, detail =f"No existe el producto con código {code}")
