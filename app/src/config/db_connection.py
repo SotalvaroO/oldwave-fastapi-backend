17663b4e7a04bc480bafd5c1b0fb0bc13cd9dc49
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-27 00:09:45 -0500

    Arreglo de db url

--- app/src/config/db_connection.py
+++ app/src/config/db_connection.py
@@ -8,7 +8,7 @@ mongo_url: str = "mongodb://localhost:27017"
 mongo_atlas_url: str = os.environ['MONGO_URL']
 
 #client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
-client = MongoClient(mongo_url)
+client = MongoClient(mongo_atlas_url)
 
 data_base = client.oldwave
 product_collection = data_base.products
